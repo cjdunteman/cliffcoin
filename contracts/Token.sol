0ca18bfff72b8c3ad10404fec4b27ba9234711fd
cjdunteman <cjdunteman@gmail.com>
2021-05-21 01:00:52 -0500

    Add pausable feature and role

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20, AccessControl {
+contract Token is ERC20, AccessControl, Pausable {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     constructor() ERC20("Cliff Coin", "CLIFF") {
         // The contract deployer can grant and ravoke any roles
@@ -20,5 +22,17 @@ contract Token is ERC20, AccessControl {
 
     function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
         _burn(from, amount);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal whenNotPaused override {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
