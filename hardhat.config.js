5c3d975a17ebb7929ee2f752b1d9f39938670d67
cjdunteman <cjdunteman@gmail.com>
2021-05-21 16:46:59 -0500

    Update dependencies and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,15 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+require("@openzeppelin/test-helpers");
+require("@openzeppelin/contracts")
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
 const { alchemyApiKey, mnemonic } = require('./secrets.json');
 
 module.exports = {
-  defaultNetwork: "ropsten",
+  defaultNetwork: "hardhat",
   networks: {
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${alchemyApiKey}`,
