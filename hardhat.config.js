6f14b04c0c44b740f78bf0bfdc6d2472c1110617
cjdunteman <cjdunteman@gmail.com>
2021-05-21 14:01:32 -0500

    Set up ropsten and alchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,15 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 
+const { alchemyApiKey, mnemonic } = require('./secrets.json');
+
 module.exports = {
+  defaultNetwork: "ropsten",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${alchemyApiKey}`,
+      accounts: {mnemonic: mnemonic}
+    },
+  },
   solidity: "0.8.0",
 };
