f96091075536572088ce29005afd8effb9dbd259
CJ Dunteman <cjdunteman@gmail.com>
2021-11-26 12:35:02 -0600

    Add snapshot functionality

--- contracts/CliffCoin.sol
+++ contracts/CliffCoin.sol
@@ -3,25 +3,24 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract CliffCoin is
-    ERC20,
-    ERC20Burnable,
-    Pausable,
-    AccessControl,
-    ERC20FlashMint
-{
+contract MyToken is ERC20, ERC20Burnable, ERC20Snapshot, AccessControl, Pausable {
+    bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Cliff Coin", "CLIFF") {
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
-        _mint(msg.sender, 1000000 * 10**decimals());
-        _setupRole(MINTER_ROLE, msg.sender);
+    constructor() ERC20("CliffCOin", "CLIFF") {
+        grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        grantRole(SNAPSHOT_ROLE, msg.sender);
+        grantRole(PAUSER_ROLE, msg.sender);
+        grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function snapshot() public onlyRole(SNAPSHOT_ROLE) {
+        _snapshot();
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -36,11 +35,11 @@ contract CliffCoin is
         _mint(to, amount);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override whenNotPaused {
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20, ERC20Snapshot)
+    {
         super._beforeTokenTransfer(from, to, amount);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,11 +2,20 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contract with the account: ", deployer.address);
+
+    const weiAmount = (await deployer.getBalance()).toString();
+
     // Deploy the contract
     const Token = await ethers.getContractFactory("CliffCoin");
+
     console.log("Deploying token...");
+
     const token = await Token.deploy();
+
     await token.deployed();
+
     console.log("CliffCoin deployed to:", token.address);
 }
 
